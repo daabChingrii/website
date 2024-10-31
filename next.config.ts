b95a729cd3fdb73a9b08e4198628f70677cbd77d
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-10-31 22:52:55 +0530

    feat: animated-tooltip-members-section

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['aceternity.com'],
+    domains: ['aceternity.com', 'images.unsplash.com'],
   },
 };
 
