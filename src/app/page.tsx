c11e6eeb7b8a78b15cc30d469d59951e231e4177
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-10-31 22:32:21 +0530

    feat: ui-about-section-with-hover-animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { HeroSection } from "@/components/HeroSection";
+import { AboutSection } from "@/components/AboutSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <HeroSection />
+    <><HeroSection /><AboutSection /></>
   );
 }
