c219764c6a91adc0c82b6ec5bc4ec92d40e9dd02
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-11-07 10:23:15 +0530

    production: try-prod-bug-fix#1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
 import Image from "next/image";
-import NavBar from "@/components/NavBar";
+// import NavBar from "@/components/NavBar";
 import { GoogleGeminiEffectDemo } from "@/components/GoogleGeminiEffectDemo";
 
 export default function Home() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       <HeroSection />
       <GoogleGeminiEffectDemo/>
       <AboutSection />
