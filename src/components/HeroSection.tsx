4786358b5bac1a37f0b5eefbb333f902ecb41637
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-11-07 09:36:04 +0530

    fix: ui-fixes-in-hero-setion-and-gemini-component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,96 +7,48 @@ export function HeroSection() {
 }
 export const products = [
   {
-    title: "Moonbeam",
-    link: "https://gomoonbeam.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+    title: "Girl in Tech",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
   {
-    title: "Cursor",
-    link: "https://cursor.so",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+    title: "",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
   {
-    title: "Rogue",
-    link: "https://userogue.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
-  },
-
-  {
-    title: "Editorially",
-    link: "https://editorially.org",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+    title: " ",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
   {
-    title: "Editrix AI",
-    link: "https://editrix.ai",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+    title: "  ",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
   {
-    title: "Pixel Perfect",
-    link: "https://app.pixelperfect.quest",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-  },
-
-  {
-    title: "Algochurn",
-    link: "https://algochurn.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+    title: "   ",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
   {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+    title: "    ",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
   {
-    title: "Tailwind Master Kit",
-    link: "https://tailwindmasterkit.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+    title: "     ",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
   {
-    title: "SmartBridge",
-    link: "https://smartbridgetech.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+    title: "      ",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
   {
-    title: "Renderwork Studio",
-    link: "https://renderwork.studio",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
-  },
-
-  {
-    title: "Creme Digital",
-    link: "https://cremedigital.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-  },
-  {
-    title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-  },
-  {
-    title: "Invoker Labs",
-    link: "https://invoker.lol",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-  },
-  {
-    title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
-    thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+    title: "        ",
+    link: "https://daabchingri.vercel.app",
+    thumbnail: "/girl-in-tech-svg.svg",
   },
 ];
