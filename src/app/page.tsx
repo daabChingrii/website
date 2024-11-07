6b681c5b3387c225e73641a368a51317b36d7c19
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-11-07 09:36:53 +0530

    feat: add-gemini-effect-with-responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,15 @@ import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
 import Image from "next/image";
 import NavBar from "@/components/NavBar";
+import { GoogleGeminiEffectDemo } from "@/components/GoogleGeminiEffectDemo";
 
 export default function Home() {
   return (
-    <><NavBar /><HeroSection /><AboutSection /></>
+    <>
+      <NavBar />
+      <HeroSection />
+      <GoogleGeminiEffectDemo/>
+      <AboutSection />
+    </>
   );
 }
